c3a5e343851fa7dcb219fbfd25a3acebf7eb4958
geowal-wondeluxe <geoffrey.walker@wondeluxe.com>
2023-05-29 13:55:18 +1000

    Add nav tree resizing functionality to nav.js

--- src/templates/nav.js
+++ src/templates/nav.js
@@ -23,14 +23,12 @@
 		{
 			if (element.classList.contains('expanded'))
 			{
-				console.log('collapse');
 				element.classList.remove('expanded');
 				element.classList.add('collapsed');
 				sessionStorage.setItem(key, 'true');
 			}
 			else if (element.classList.contains('collapsed'))
 			{
-				console.log('expand');
 				element.classList.remove('collapsed');
 				element.classList.add('expanded');
 				sessionStorage.removeItem(key);
@@ -41,23 +39,92 @@
 
 (function()
 {
+	let menu = document.querySelector('#nav-tree');
 	let handle = document.querySelector('#nav-handle');
+	let content = document.querySelector('#content');
+	let drag = null;
 
-	// Need to capture bound function.
-	// handle.addEventListener('mousedown', onHandleDown.bind(this));
+	const MIN_WIDTH = menu.offsetWidth;
+	const MAX_WIDTH = content.offsetWidth;
+
+	let menuWidth = sessionStorage.getItem('menu.width');
+
+	console.log('menu.width', menuWidth);
+
+	if (menuWidth)
+	{
+		// menu.style.width = menuWidth;
+		// menu.style.setProperty('width', menuWidth);
+
+		menu.style.setProperty('flex-basis', menuWidth);
+
+		// console.log('menu.style.width', menu.style.width);
+		// console.log('menu.offsetWidth', menu.offsetWidth);
+
+		// setTimeout(() => {
+		// 	menu.style.setProperty('width', menuWidth);
+		// 	// console.log('setMenuWidth', menu.style.width);
+		// 	// console.log('menu.offsetWidth', menu.offsetWidth);
+		// }, 1000);
+	}
+
+	handle.addEventListener('mousedown', onHandleDown);
 
 	function onHandleDown(evt)
 	{
-		// TODO
+		// console.log('onHandleDown', evt);
+		// console.log('menu', menu);
+
+		window.addEventListener('mousemove', onMouseMove);
+		window.addEventListener('mouseup', onMouseUp);
+
+		drag = {
+			originX: evt.screenX,
+			originWidth: menu.offsetWidth
+		};
+
+		console.log('drag', drag);
 	}
 
 	function onMouseMove(evt)
 	{
+		// let dragWidth = Math.floor(drag.originWidth + evt.screenX - drag.originX);
+		let dragWidth = drag.originWidth + evt.screenX - drag.originX;
 
+		// menuWidth = Math.max(Math.floor(drag.originWidth + evt.screenX - drag.originX), MIN_WIDTH) + 'px';
+		// menu.style.width = menuWidth;
+
+		menuWidth = ((dragWidth < MIN_WIDTH) ? MIN_WIDTH : (dragWidth > MAX_WIDTH) ? MAX_WIDTH : dragWidth) + 'px';
+
+		// menu.style.setProperty('width', menuWidth);
+		menu.style.setProperty('flex-basis', menuWidth);
+
+		// if (menu.offsetWidth != parseInt(menuWidth))
+		// {
+		// 	console.warn('Target width (' + menuWidth + ') not set. Use value:', menu.offsetWidth);
+		// }
+
+		sessionStorage.setItem('menu.width', menuWidth);
+		evt.preventDefault();
 	}
 
 	function onMouseUp(evt)
 	{
+		// console.log('onMouseUp', evt);
 
+		onMouseMove(evt);
+
+		console.log('storedWidth', sessionStorage.getItem('menu.width'));
+		console.log('offsetWidth', menu.offsetWidth);
+
+		window.removeEventListener('mousemove', onMouseMove);
+		window.removeEventListener('mouseup', onMouseUp);
+
+		drag = null;
+	}
+
+	function clamp(value, min, max)
+	{
+		return (value < min) ? min : (value > max) ? max : value;
 	}
 })();
