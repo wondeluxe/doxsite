c98101c0b66d224564b8be02a5d137a288f37206
geowal-wondeluxe <geoffrey.walker@wondeluxe.com>
2023-05-30 16:20:02 +1000

    Remove comments from nav.js

--- src/templates/nav.js
+++ src/templates/nav.js
@@ -49,32 +49,15 @@
 
 	let menuWidth = sessionStorage.getItem('menu.width');
 
-	console.log('menu.width', menuWidth);
-
 	if (menuWidth)
 	{
-		// menu.style.width = menuWidth;
-		// menu.style.setProperty('width', menuWidth);
-
 		menu.style.setProperty('flex-basis', menuWidth);
-
-		// console.log('menu.style.width', menu.style.width);
-		// console.log('menu.offsetWidth', menu.offsetWidth);
-
-		// setTimeout(() => {
-		// 	menu.style.setProperty('width', menuWidth);
-		// 	// console.log('setMenuWidth', menu.style.width);
-		// 	// console.log('menu.offsetWidth', menu.offsetWidth);
-		// }, 1000);
 	}
 
 	handle.addEventListener('mousedown', onHandleDown);
 
 	function onHandleDown(evt)
 	{
-		// console.log('onHandleDown', evt);
-		// console.log('menu', menu);
-
 		window.addEventListener('mousemove', onMouseMove);
 		window.addEventListener('mouseup', onMouseUp);
 
@@ -82,49 +65,26 @@
 			originX: evt.screenX,
 			originWidth: menu.offsetWidth
 		};
-
-		console.log('drag', drag);
 	}
 
 	function onMouseMove(evt)
 	{
-		// let dragWidth = Math.floor(drag.originWidth + evt.screenX - drag.originX);
 		let dragWidth = drag.originWidth + evt.screenX - drag.originX;
-
-		// menuWidth = Math.max(Math.floor(drag.originWidth + evt.screenX - drag.originX), MIN_WIDTH) + 'px';
-		// menu.style.width = menuWidth;
 
 		menuWidth = ((dragWidth < MIN_WIDTH) ? MIN_WIDTH : (dragWidth > MAX_WIDTH) ? MAX_WIDTH : dragWidth) + 'px';
 
-		// menu.style.setProperty('width', menuWidth);
 		menu.style.setProperty('flex-basis', menuWidth);
-
-		// if (menu.offsetWidth != parseInt(menuWidth))
-		// {
-		// 	console.warn('Target width (' + menuWidth + ') not set. Use value:', menu.offsetWidth);
-		// }
-
 		sessionStorage.setItem('menu.width', menuWidth);
 		evt.preventDefault();
 	}
 
 	function onMouseUp(evt)
 	{
-		// console.log('onMouseUp', evt);
-
 		onMouseMove(evt);
-
-		console.log('storedWidth', sessionStorage.getItem('menu.width'));
-		console.log('offsetWidth', menu.offsetWidth);
 
 		window.removeEventListener('mousemove', onMouseMove);
 		window.removeEventListener('mouseup', onMouseUp);
 
 		drag = null;
-	}
-
-	function clamp(value, min, max)
-	{
-		return (value < min) ? min : (value > max) ? max : value;
 	}
 })();
